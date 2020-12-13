4d7a1729a63beeb4b34b06c94fd4fbbb155f50f2
Nataliia Cherkes <69033364+chernataly2020@users.noreply.github.com>
2020-12-13 02:22:07 +0200

    feat: solve task "06-object-tasks"

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+    return {
+        width,
+        height,
+        getArea() {
+            return this.width * this.height;
+        },
+    };
 }
 
 
@@ -35,8 +41,11 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+    /*Метод JSON.stringify() преобразует значение JavaScript в строку JSON,
+    возможно с заменой значений, если указана функция замены,
+    или с включением только определённых свойств, если указан массив замены.*/
+    return JSON.stringify(obj);
 }
 
 
@@ -51,8 +60,18 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+    //Метод JSON.parse() разбирает строку JSON,
+    //возможно с преобразованием получаемого в процессе разбора значения.
+    const obj = JSON.parse(json);
+
+    //Метод  Object.values() возвращает массив значений перечисляемых свойств объекта в том же порядке что и цикл for...in.
+    //Разница между циклом и методом в том, что цикл перечисляет свойства и из цепочки прототипов.
+    const values = Object.values(obj);
+
+    //Возвращает ссылку на функцию Object, создавшую прототип экземпляра.
+    //Все объекты наследуют свойство constructor из своего прототипа
+    return new proto.constructor(...values);
 }
 
 
@@ -111,39 +130,79 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
-  },
+    answer: '',
+    element: function(value) {
+        this.error(1);
 
-  id(/* value */) {
-    throw new Error('Not implemented');
-  },
+        //Метод Object.create() создаёт новый объект с указанным прототипом и свойствами.
 
-  class(/* value */) {
-    throw new Error('Not implemented');
-  },
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 1;
+        obj.answer = this.answer + value;
+        return obj;
+    },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
-  },
+    id: function(value) {
+        this.error(2);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 2;
+        obj.answer = this.answer + '#' + value;
+        return obj;
+    },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
-  },
+    class: function(value) {
+        this.error(3);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 3;
+        obj.answer = this.answer + '.' + value;
+        return obj;
+    },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
-  },
+    attr: function(value) {
+        this.error(4);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 4;
+        obj.answer = this.answer + '[' + value + ']';
+        return obj;
+    },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
-  },
+    pseudoClass: function(value) {
+        this.error(5);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 5;
+        obj.answer = this.answer + ':' + value;
+        return obj;
+    },
+
+    pseudoElement: function(value) {
+        this.error(6);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 6;
+        obj.answer = this.answer + '::' + value;
+        return obj;
+    },
+
+    combine: function(selector1, combinator, selector2) {
+        let obj = Object.create(cssSelectorBuilder);
+        obj.answer = selector1.answer + ' ' + combinator + ' ' + selector2.answer;
+        return obj;
+    },
+    stringify: function() {
+        return this.answer;
+    },
+
+    //Обработка ошибок
+    error: function(nom_i) {
+        if (this.i > nom_i) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+        if (this.i == nom_i && (nom_i == 1 || nom_i == 2 || nom_i == 6)) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    },
 };
 
 
 module.exports = {
-  Rectangle,
-  getJSON,
-  fromJSON,
-  cssSelectorBuilder,
+    Rectangle,
+    getJSON,
+    fromJSON,
+    cssSelectorBuilder,
 };
+
